541d883c64b79a806b0dab1bba5b48b9c2d7ed94
PriyatoshKumarShahi <priytoshshahi90@gmail.com>
2026-06-26 15:17:53 +0530

    Adding changes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,10 @@ const app = express();
 connectDB();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
-app.use(cors({ origin: process.env.CLIENT_URL?.split(',') || ['http://localhost:5173'], credentials: true }));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials:true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
